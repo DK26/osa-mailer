04c0295b152589848d5d82e1b9bf382c4ae0098c
David Krasnitsky <dikaveman@gmail.com>
2022-08-22 14:06:31 +0300

    Reduced allocations by using references when reading from an array of strings coming from Value object

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-use crc::{Algorithm, Crc, Digest, CRC_32_ISO_HDLC};
+use crc::{Algorithm, Crc, CRC_32_ISO_HDLC};
 
 // CRC_32_ISO_HDLC is compatible with Python 3
 const CRC32_ALGORITHM: Algorithm<u32> = CRC_32_ISO_HDLC;
@@ -11,14 +11,14 @@ struct Email<'json_entry> {
     system: &'json_entry str,
     subsystem: &'json_entry str,
     from: &'json_entry str,
-    to: Vec<String>,
-    cc: Vec<String>,
-    bcc: Vec<String>,
-    reply_to: Vec<String>,
+    to: Vec<&'json_entry str>,
+    cc: Vec<&'json_entry str>,
+    bcc: Vec<&'json_entry str>,
+    reply_to: Vec<&'json_entry str>,
     subject: &'json_entry str,
     template: &'json_entry str,
     alternative_content: &'json_entry str,
-    attachments: Vec<PathBuf>,
+    attachments: Vec<&'json_entry Path>,
     // custom_key: &'json_entry str,
 }
 
@@ -32,6 +32,9 @@ enum EntryError {
 
     #[error("The field `{0}` is containing a wrong type")]
     WrongFieldType(&'static str),
+
+    #[error("Wrong item type in array `{0}`")]
+    WrongArrayItem(&'static str),
 }
 
 impl<'json_entry> TryFrom<&'json_entry serde_json::Value> for Email<'json_entry> {
@@ -46,17 +49,17 @@ impl<'json_entry> TryFrom<&'json_entry serde_json::Value> for Email<'json_entry>
         let subsystem = get_str_value(email, "subsystem")?;
         let from = get_str_value(email, "from")?;
 
-        let to = get_vec_value(email, "to")?;
-        let cc = get_vec_value(email, "cc")?;
-        let bcc = get_vec_value(email, "bcc")?;
-        let reply_to = get_vec_value(email, "reply_to")?;
+        let to = get_str_vec_value(email, "to")?;
+        let cc = get_str_vec_value(email, "cc")?;
+        let bcc = get_str_vec_value(email, "bcc")?;
+        let reply_to = get_str_vec_value(email, "reply_to")?;
 
         let subject = get_str_value(email, "subject")?;
         let template = get_str_value(email, "template")?;
 
         let alternative_content = get_str_value(email, "alternative_content")?;
 
-        let attachments = get_paths_value(email, "attachments")?;
+        let attachments = get_path_vec_value(email, "attachments")?;
 
         let email_checksum = crc32_iso_hdlc_checksum(email.to_string().as_bytes());
         let id = format!("{:x}", email_checksum);
@@ -107,29 +110,44 @@ fn get_str_value<'json_entry>(
     Ok(result)
 }
 
-fn get_vec_value(value: &serde_json::Value, key: &'static str) -> Result<Vec<String>, EntryError> {
-    Ok(value
+fn get_str_vec_value<'json_entry>(
+    value: &'json_entry serde_json::Value,
+    key: &'static str,
+) -> Result<Vec<&'json_entry str>, EntryError> {
+    value
         .get(key)
         .ok_or(EntryError::MissingField(key))?
         .as_array()
         .ok_or(EntryError::WrongFieldType(key))?
         .iter()
-        .map(|v| v.to_string())
-        .collect())
+        .map(|v| {
+            if let serde_json::Value::String(ref iv) = v {
+                Ok(iv.as_str())
+            } else {
+                Err(EntryError::WrongArrayItem(key))
+            }
+        })
+        .collect()
 }
 
-fn get_paths_value(
-    value: &serde_json::Value,
+fn get_path_vec_value<'json_entry>(
+    value: &'json_entry serde_json::Value,
     key: &'static str,
-) -> Result<Vec<PathBuf>, EntryError> {
-    Ok(value
+) -> Result<Vec<&'json_entry Path>, EntryError> {
+    value
         .get(key)
         .ok_or(EntryError::MissingField(key))?
         .as_array()
         .ok_or(EntryError::WrongFieldType(key))?
         .iter()
-        .map(|v| PathBuf::from(v.to_string()))
-        .collect())
+        .map(|v| {
+            if let serde_json::Value::String(ref iv) = v {
+                Ok(iv.as_ref())
+            } else {
+                Err(EntryError::WrongArrayItem(key))
+            }
+        })
+        .collect()
 }
 
 fn main() -> anyhow::Result<()> {
