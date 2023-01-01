7163df1b469d1d1847d6bbd4f668e04001cf0844
David Krasnitsky <dikaveman@gmail.com>
2023-01-01 18:32:20 +0200

    Fixed: Bad logs output for `mail-script.cmd`

--- mail-script.cmd
+++ mail-script.cmd
@@ -10,7 +10,7 @@ SET AUTH=noauth
 ::SET PASSWORD=password
 
 :: Prep Log
-SET LOG_FILE=logs\%date:~7,2%-%date:~4,2%-%date:~-4%_%time:~0,2%%time:~3,2%%time:~6,2%.log
+SET LOG_FILE="logs\%date:~7,2%-%date:~4,2%-%date:~-4%_%time:~0,2%%time:~3,2%%time:~6,2%.log"
 MKDIR logs 2> NUL
 
 :: Run & Log to file
