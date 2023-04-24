7048d61e1e0ce3cc4fd90d6a6aa83bbd6c0748b3
David Krasnitsky <dikaveman@gmail.com>
2023-04-24 20:29:41 +0300

    Added: `ErrorReport` type

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 #[derive(thiserror::Error, Debug)]
 pub enum EntryError {
     #[error("Entry does not contain `email` section")]
@@ -18,4 +20,40 @@ pub enum EntryError {
         content: String,
         error: serde_json::Error,
     },
+}
+
+#[derive(Debug, Default)]
+pub struct ErrorReport {
+    /// Additional context for the errors, such as JSON file contents
+    context: Option<String>,
+
+    /// Errors regarding a specific context, such as multiple detected error in a JSON file.
+    errors: Vec<Box<dyn Error>>,
+}
+
+impl ErrorReport {
+    #[inline]
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    #[inline]
+    pub fn add_error<E: Error + 'static>(&mut self, error: E) {
+        self.errors.push(Box::new(error));
+    }
+
+    #[inline]
+    pub fn set_context(&mut self, context: String) {
+        self.context = Some(context)
+    }
+
+    #[inline]
+    pub fn context(&self) -> Option<&str> {
+        self.context.as_deref()
+    }
+
+    #[inline]
+    pub fn errors(&self) -> &[Box<dyn Error>] {
+        self.errors.as_slice()
+    }
 }
