6cd4fe405fa65eab3003161b6f1234dd6f226265
David Krasnitsky <dikaveman@gmail.com>
2023-04-25 18:06:15 +0300

    Fully implemented the `ErrorReport` type

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,8 @@
+#![allow(dead_code)]
+
 use std::error::Error;
+
+use chrono::{DateTime, Utc};
 
 #[derive(thiserror::Error, Debug)]
 pub enum EntryError {
@@ -22,13 +26,33 @@ pub enum EntryError {
     },
 }
 
+#[derive(Debug)]
+pub struct ErrorEvent(DateTime<Utc>, Box<dyn Error + Send + Sync + 'static>);
+
+impl<T: Error + Send + Sync + 'static> From<T> for ErrorEvent {
+    fn from(error: T) -> Self {
+        ErrorEvent(chrono::offset::Utc::now(), Box::new(error))
+    }
+}
+
+/// There could be a special case where an error type is not implementing the `std::error::Error` type.
+/// For these cases, you'll have to use this `ErrorWrapper<E>` and maybe implement your own `From<ErrorWrapper<E>>` for
+/// the `ErrorEvent` type. Currently this is used to wrap the `anyhow::Error` type.
+pub struct ErrorWrapper<E>(E);
+
+impl From<ErrorWrapper<anyhow::Error>> for ErrorEvent {
+    fn from(error: ErrorWrapper<anyhow::Error>) -> Self {
+        ErrorEvent(chrono::offset::Utc::now(), error.0.into())
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct ErrorReport {
     /// Additional context for the errors, such as JSON file contents
     context: Option<String>,
 
     /// Errors regarding a specific context, such as multiple detected error in a JSON file.
-    errors: Vec<Box<dyn Error>>,
+    errors: Vec<ErrorEvent>,
 }
 
 impl ErrorReport {
@@ -38,13 +62,15 @@ impl ErrorReport {
     }
 
     #[inline]
-    pub fn add_error<E: Error + Send + Sync + 'static>(&mut self, error: E) {
-        self.errors.push(Box::new(error));
+    pub fn add_error<E: Into<ErrorEvent>>(mut self, error: E) -> Self {
+        self.errors.push(error.into());
+        self
     }
 
     #[inline]
-    pub fn set_context(&mut self, context: String) {
-        self.context = Some(context)
+    pub fn set_context(mut self, context: String) -> Self {
+        self.context = Some(context);
+        self
     }
 
     #[inline]
@@ -53,7 +79,44 @@ impl ErrorReport {
     }
 
     #[inline]
-    pub fn errors(&self) -> &[Box<dyn Error>] {
+    pub fn errors(&self) -> &[ErrorEvent] {
         self.errors.as_slice()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::anyhow;
+
+    #[test]
+    fn test_error_report() {
+        let error_report = ErrorReport::new()
+            .set_context("test_file.json".to_string())
+            .add_error(EntryError::MissingField("unique_by"))
+            .add_error(EntryError::MissingEmailSection)
+            .add_error(ErrorWrapper(anyhow!("anyhow error")));
+
+        let mut errors_iter = error_report.errors().iter();
+
+        let ErrorEvent(_timestamp, error) = errors_iter.next().unwrap();
+
+        assert_eq!(
+            error.to_string(),
+            EntryError::MissingField("unique_by").to_string()
+        );
+
+        let ErrorEvent(_timestamp, error) = errors_iter.next().unwrap();
+
+        assert_eq!(
+            error.to_string(),
+            EntryError::MissingEmailSection.to_string()
+        );
+
+        let ErrorEvent(_timestamp, error) = errors_iter.next().unwrap();
+
+        assert_eq!(error.to_string(), "anyhow error".to_string());
+
+        assert_eq!(error_report.context(), Some("test_file.json"));
     }
 }
